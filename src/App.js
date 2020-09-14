43a45e101248f07c11356e32f4cf5d8654964a4c
Your Name <you@example.com>
2020-09-14 14:12:53 +0530

    fc

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import  React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Profile from './pages/Profile';
+import Dashboard from './pages/Dashboard';
+
+class  App extends Component {
+  render(){
+
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+          </ul>
+  
+          <hr />
+  
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/contact">
+              <Contact/>
+            </Route>
+            <Route path="/profile">
+              <Profile/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
+}
+
+// You can think of these components as "pages"
+// in your app.
+
+export default App;
