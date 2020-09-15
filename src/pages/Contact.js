24e86764270ca853db1ffe43adfedc1628705917
Your Name <you@example.com>
2020-09-15 12:42:24 +0530

    fc

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,101 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 
-export default class Contact extends Component{
-    render(){
-        return(
-            <>
-            contact
-            </>
+export default class Contact extends Component {
+    state ={
+        address:"",
+        address2:"",
+        city:"",
+        state:"",
+        zip:"",
+    }
+    handleAddress(e){
+        console.log(e)
+        const address= (e.target.value)
+        this.setState(prevState =>{         
+            prevState.address=address;
+            return prevState;
+        })
+    }
+    handleAddress2(e){
+        const address2=(e.target.value)
+        this.setState(prevState =>{
+            prevState.address2=address2;
+            return prevState;
+        })
+    }
+    handleCity(e){
+        const city=(e.target.value)
+        this.setState(prevState =>{
+            prevState.city=city;
+            return prevState;
+        })
+    }
+    handleState(e){
+        const state=(e.target.value)
+        this.setState(prevState =>{
+            prevState.state=state;
+            return prevState;
+        })
+    }
+    handleZip(e){
+        const zip=(e.target.value)
+        this.setState(prevState =>{
+            prevState.zip=zip;
+            return prevState;
+        })
+    }
+    handleCheckbox(e) {
+        console.log(e)
+        const checkbox=(e.target.checked)
+        this.setState({
+            checkbox: checkbox
+        })
+    }
+   
+    render() {
+        return (
+            <div className="container">
+                <form>
+
+                    <div className="form-group">
+                        <label htmlFor="inputAddress">Address</label>
+                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" onChange={this.handleAddress.bind(this)}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="inputAddress2">Address 2</label>
+                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"onChange={this.handleAddress2.bind(this)} />
+                    </div>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label htmlFor="inputCity">City</label>
+                            <input type="text" className="form-control" id="inputCity" onChange={this.handleCity.bind(this)}/>
+                        </div>
+                        <div className="form-group col-md-4">
+                            <label htmlFor="inputState">State</label>
+                            <select id="inputState" className="form-control" onChange={this.handleState.bind(this)}>
+                                <option value="0">Choose State</option>
+                                <option value="1" >Bihar</option>
+                                <option value="2">Jharkhand</option>
+                            </select>
+                        </div>
+                        <div className="form-group col-md-2">
+                            <label htmlFor="inputZip">Zip</label>
+                            <input type="text" className="form-control" id="inputZip" onChange={this.handleZip.bind(this)} />
+                        </div>
+                    </div>
+                    <div className="form-group">
+                        <div className="form-check">
+                            <input className="form-check-input" type="checkbox" id="gridCheck" onChange={this.handleCheckbox.bind(this)}/>
+                            <label className="form-check-label" htmlFor="gridCheck">
+                                Check me out
+                            </label>
+                        </div>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Sign in</button>
+                </form>
+                {this.state.address},{this.state.address2},{this.state.city},{this.state.state},
+                {this.state.zip}{this.state.checkbox}
+            </div>
         )
     }
 }
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,70 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 
-export default class Profile extends Component{
-    render(){
-        return(
-            <>
-            profile
-            </>
+export default class Profile extends Component {
+
+    state = {
+       firstname:"",
+       lastname:"",
+       email:"",
+       password:""
+    }
+    handleFirstname(e){
+        console.log(e)
+        const firstname= (e.target.value)
+        this.setState(prevState =>{         
+            prevState.firstname=firstname;
+            return prevState;
+        })
+    }
+    handleLasttname(e){
+        console.log(e)
+        const lastname =(e.target.value)
+        this.setState(prevState =>{
+            prevState.lastname=lastname;
+            return prevState;
+        })
+    }
+    handleEmail(e){
+        console.log(e)
+        const email=(e.target.value)
+        this.setState(prevState =>{
+            prevState.email=email;
+            return prevState;
+        })
+    }
+    handlePassword(e){
+        console.log(e)
+        const password=(e.target.value)
+        this.setState(prevState =>{
+            prevState.password=password;
+            return prevState;
+        })
+    }
+   
+    
+    render() {
+        return (
+            <div className="container">
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                        <input type="text" className="form-control" placeholder="First name" onChange={this.handleFirstname.bind(this)} />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <input type="text" className="form-control" placeholder="Last name" onChange={this.handleLasttname.bind(this)} />
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="inputEmail4">Email</label>
+                        <input type="email" className="form-control" id="inputEmail4" onChange={this.handleEmail.bind(this)}/>
+                    </div>
+                    <div className="form-group col-md-6">
+                        <label htmlFor="inputPassword4">Password</label>
+                        <input type="password" className="form-control" id="inputPassword4" onChange={this.handlePassword.bind(this)}/>
+                    </div>
+                </div>
+                {this.state.firstname }, {this.state.lastname},{this.state.email},{this.state.password}
+
+            </div>
+
         )
     }
 }
