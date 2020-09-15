6df3dce2c0b7068c55264a44811afcd2deb2b7aa
Your Name <you@example.com>
2020-09-15 14:07:36 +0530

    fc

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,57 +1,67 @@
 import React, { Component } from "react";
 
 export default class Contact extends Component {
-    state ={
-        address:"",
-        address2:"",
-        city:"",
-        state:"",
-        zip:"",
+    state = {
+        address: "",
+        address2: "",
+        city: "",
+        state: "",
+        zip: "",
+    };
+    componentDidMount() {
+        const foundState = localStorage.getItem("contack")
+        console.log(foundState)
+        if (foundState != null) {
+            const foundStateObject = JSON.parse(foundState);
+            this.setState(prevState => {
+                return foundStateObject;
+            })
+        }
     }
-    handleAddress(e){
+    handleAddress(e) {
         console.log(e)
-        const address= (e.target.value)
-        this.setState(prevState =>{         
-            prevState.address=address;
+        const address = (e.target.value)
+        this.setState(prevState => {
+            prevState.address = address;
             return prevState;
         })
     }
-    handleAddress2(e){
-        const address2=(e.target.value)
-        this.setState(prevState =>{
-            prevState.address2=address2;
+    handleAddress2(e) {
+        const address2 = (e.target.value)
+        this.setState(prevState => {
+            prevState.address2 = address2;
             return prevState;
         })
     }
-    handleCity(e){
-        const city=(e.target.value)
-        this.setState(prevState =>{
-            prevState.city=city;
+    handleCity(e) {
+        const city = (e.target.value)
+        this.setState(prevState => {
+            prevState.city = city;
             return prevState;
         })
     }
-    handleState(e){
-        const state=(e.target.value)
-        this.setState(prevState =>{
-            prevState.state=state;
+    handleState(e) {
+        const state = (e.target.value)
+        this.setState(prevState => {
+            prevState.state = state;
             return prevState;
         })
     }
-    handleZip(e){
-        const zip=(e.target.value)
-        this.setState(prevState =>{
-            prevState.zip=zip;
+    handleZip(e) {
+        const zip = (e.target.value)
+        this.setState(prevState => {
+            prevState.zip = zip;
             return prevState;
         })
     }
     handleCheckbox(e) {
         console.log(e)
-        const checkbox=(e.target.checked)
+        const checkbox = (e.target.checked)
         this.setState({
             checkbox: checkbox
         })
     }
-   
+
     render() {
         return (
             <div className="container">
@@ -59,20 +69,26 @@ export default class Contact extends Component {
 
                     <div className="form-group">
                         <label htmlFor="inputAddress">Address</label>
-                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" onChange={this.handleAddress.bind(this)}/>
+                        <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"
+                            value={this.state.address}
+                            onChange={this.handleAddress.bind(this)} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="inputAddress2">Address 2</label>
-                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"onChange={this.handleAddress2.bind(this)} />
+                        <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"
+                            value={this.state.address2}
+                            onChange={this.handleAddress2.bind(this)} />
                     </div>
                     <div className="form-row">
                         <div className="form-group col-md-6">
                             <label htmlFor="inputCity">City</label>
-                            <input type="text" className="form-control" id="inputCity" onChange={this.handleCity.bind(this)}/>
+                            <input type="text" className="form-control" id="inputCity"
+                            value={this.state.city} onChange={this.handleCity.bind(this)} />
                         </div>
                         <div className="form-group col-md-4">
                             <label htmlFor="inputState">State</label>
-                            <select id="inputState" className="form-control" onChange={this.handleState.bind(this)}>
+                            <select id="inputState" className="form-control"
+                            value={this.state.state} onChange={this.handleState.bind(this)}>
                                 <option value="0">Choose State</option>
                                 <option value="1" >Bihar</option>
                                 <option value="2">Jharkhand</option>
@@ -80,12 +96,13 @@ export default class Contact extends Component {
                         </div>
                         <div className="form-group col-md-2">
                             <label htmlFor="inputZip">Zip</label>
-                            <input type="text" className="form-control" id="inputZip" onChange={this.handleZip.bind(this)} />
+                            <input type="text" className="form-control" id="inputZip"
+                            value={this.state.zip} onChange={this.handleZip.bind(this)} />
                         </div>
                     </div>
                     <div className="form-group">
                         <div className="form-check">
-                            <input className="form-check-input" type="checkbox" id="gridCheck" onChange={this.handleCheckbox.bind(this)}/>
+                            <input className="form-check-input" type="checkbox" id="gridCheck" onChange={this.handleCheckbox.bind(this)} />
                             <label className="form-check-label" htmlFor="gridCheck">
                                 Check me out
                             </label>
