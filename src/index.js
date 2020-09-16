a4677edf9e75612e20e1d4d2c70b865c6fccf55c
Your Name <you@example.com>
2020-09-16 12:54:40 +0530

    strickmmode comment becouse  setstate was called twice

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
