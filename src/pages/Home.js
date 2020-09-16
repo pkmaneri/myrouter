4a0721dd1529fa25c2f727812354c728d8ace934
Your Name <you@example.com>
2020-09-16 12:33:30 +0530

    product added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,41 @@
-import React,{ Component } from "react";
+import React, { Component } from "react";
 
-export default class Home extends Component{
-    render(){
-        return(
-            <>
-            home
-            </>
+export default class Home extends Component {
+    state= {
+        "product":[]
+    }
+    componentDidMount() {
+        const foundState = localStorage.getItem("product")
+        console.log(foundState)
+        if (foundState != null) {
+            const foundStateObject = JSON.parse(foundState);
+            this.setState(prevState => {
+                prevState.product= foundStateObject
+                return prevState;
+            })
+        }
+    }
+
+    render() {
+        console.log(typeof this.state)
+        return (
+            this.state.product.map((product,i)=>{
+
+                return(
+                    <div className="card" key={i}>
+                    <div className="card-body">
+                        <h5 className="card-title">{product.name}</h5>
+                        <h6 className="card-subtitle mb-2 text-muted">{product.price}</h6>
+                        <p className="card-text">
+                            {product.category}
+                        </p>
+
+                    </div>
+                </div>
+                )
+
+            })
+       
         )
     }
 }
