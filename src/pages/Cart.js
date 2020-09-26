1c9b8f5cbb9f67d7aaf73b8a253d0e44ead19dd3
Your Name <you@example.com>
2020-09-26 22:43:58 +0530

    butto increment added

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,59 +1,103 @@
 import React, { Component } from "react"
+import "./Cart.css"
 
 export default class Cart extends Component {
     state = {
         "productAddedToCart": [],
-
-
+        total:0,
     }
+    
+    reducer = (accumulator, product) =>  parseFloat( accumulator.price ) * accumulator.quantity + parseFloat( product.price ) * product.quantity;
+
     componentDidMount() {
         const foundState = localStorage.getItem("productAddedToCart")
         console.log(foundState)
         if (foundState != null) {
             const foundStateObject = JSON.parse(foundState);
+            foundStateObject.map((product)=>{
+                if(!product.quantity){
+                    product.quantity = 1
+                }
+                return product;
+            })
+                        
             this.setState(prevState => {
                 return {
-                    "productAddedToCart": foundStateObject
+                    "productAddedToCart": foundStateObject,
+                    "total": foundStateObject.reduce(this.reducer)
                 };
             })
         }
     }
-    handleAmount(e) {
-        const amount = (e.target.value)
+
+    handleIncrement( i){
+        let productAddedToCart = [...this.state.productAddedToCart];
+
+        let product = {...this.state.productAddedToCart[i]};
+        product.quantity = product.quantity + 1;
+
+        productAddedToCart[i] = product;
+
         this.setState(prevState => {
-
             return {
-                currentData: {
-                    amount: amount,
-                    productAddedToCart: prevState.currentData.productAddedToCart
-
-                }
-            }
+                "productAddedToCart": productAddedToCart ,
+                "total": productAddedToCart.reduce(this.reducer)
+            };
+        }, () => {
+            const productdataArrstate = JSON.stringify(this.state.productAddedToCart);
+            localStorage.setItem("productAddedToCart", productdataArrstate)
         })
+
     }
-
-
 
     render() {
         console.log(typeof this.state)
 
         return (
-            this.state.productAddedToCart.map((product, i) => {
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>
+                            Name
+                        </th>
+                        <th>
+                            Quantity
+                        </th>
+                        <th>
+                            Price
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.state.productAddedToCart.map((product, i) => {
+                    return(
+                    <tr>
+                        <td> 
+                            {product.name}
+                        </td>
+                        <td>
+                            <button >-</button>
+                            {product.quantity}
+                            <button onClick={this.handleIncrement.bind(this,  i)}>+</button>
+                        </td>
+                        <td>
+                            {product.price}
+                        </td>
+                    </tr>
+                    )
+                })}
+                <tr>
+                    <td colSpan="2" className="total">
+                        Total:
+                    </td>
+                    <td>
+                        {this.state.total}
+                    </td>
+                </tr>
 
-                console.log(product)
-                return (
+                </tbody>
+            </table>
 
-                    <div className="card" key={i}>
-                        <div className="card-body">
-                            <h2 className="card-subtitle mb-2 text-muted">{product.name}
-                            {product.price}</h2>
-                            {/* <h3 className="card-subtitle mb-2 text-muted">{product.price}</h3> */}
-                        </div>
-                    </div>
-
-
-                )
-            })
 
         )
     }
